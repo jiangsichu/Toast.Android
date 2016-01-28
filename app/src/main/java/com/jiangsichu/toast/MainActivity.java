0e2debfebb2ffe46055e5a374e1a2c8cc238b833
Sichu Jiang <jiangsichu@gmail.com>
2016-01-28 21:58:08 +0000

    Add un-functional view components for search

--- app/src/main/java/com/jiangsichu/toast/MainActivity.java
+++ app/src/main/java/com/jiangsichu/toast/MainActivity.java
@@ -3,11 +3,13 @@ package com.jiangsichu.toast;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        // Configure the search info and add any event listeners...
+
         return true;
     }
 
